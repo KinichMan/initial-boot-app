e9fcb075dc4c62b8b426304014c8aaefc44a46b2
KinichMan <kinichman@gmail.com>
2022-05-12 20:12:14 -0500

    Configuring embedded Tomcat

--- src/main/java/com/uma/initialbootapp/InitialBootAppApplication.java
+++ src/main/java/com/uma/initialbootapp/InitialBootAppApplication.java
@@ -3,12 +3,11 @@ package com.uma.initialbootapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
 public class InitialBootAppApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(InitialBootAppApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(InitialBootAppApplication.class, args);
+    }
 
 }
